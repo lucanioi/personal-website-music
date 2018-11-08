efe3b6f1fa6fc7101999507899af6d96f1160b3c
Luca Nioi <lucahidekinioi@gmail.com>
2018-11-08 02:20:54 +0100

    add google verification html to root

--- website.rb
+++ website.rb
@@ -6,3 +6,9 @@ get '/' do
   video_contents = ContentLoader.load_contents('./public/contents/video.contents')
   erb :index, locals: {videos: video_contents}
 end
+
+# google site verification
+
+get '/google47864032136ca1ca.html' do
+  send_file './google47864032136ca1ca.html'
+end
